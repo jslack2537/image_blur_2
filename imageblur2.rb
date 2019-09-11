182420da450e4df8e1abad7fa5d80474e3ad11ba
Charles <jslack2537@gmail.com>
2019-09-11 17:26:30 +0000

    finished logic to blur images

--- imageblur2.rb
+++ imageblur2.rb
@@ -1,35 +1,59 @@
-
-
-#image blur 2
+#image-blur2
 class Image
-    attr_accessor :image
 
-    def initialize(image)
+  def initialize(image)
+    @image = image
+    @row_length = image.length
+    @col_length = image[0].length
 
-          @image = image
+  end 
+
+  def output_image
+    @image.each do |row|
+      puts row.join
     end
+  end
 
-  
-    def output_image
-          @image.each do |row|
-  
-            puts row.join
-    end
+  def blur(row_index, col_index)
+    @image[row_index -1][col_index] = 1 unless row_index == 0
+    @image[row_index +1][col_index] = 1 unless row_index >= @row_length-1
+    @image[row_index][col_index -1] = 1 unless col_index == 0
+    @image[row_index][col_index +1] = 1 unless col_index >= @col_length-1
+  end
+
+  def find_ones
+    one_pixel = []
+
+    @image.each_with_index do |row_array, row_index|
+        row_array.each_with_index do |cell, col_index|
+        if cell == 1
+          one_pixel << [row_index, col_index]
+        end
+      end
+    end    
+    one_pixel  
+  end
 
 
-end
+  def change
+    image_cordinates = find_ones
+
+    image_cordinates.each do |x, y|
+      blur(x, y)
+    end  
+  end
+
+
 end
 
 
 image = Image.new([
-
-[0, 0, 0, 0],
-[0, 1, 0, 0],
-[0, 0, 0, 1],
-[0, 0, 0, 0]
-
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
 ])
 
 
-
+image.change
 image.output_image
